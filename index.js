bfdb158b719b84fc8c867ebe5fcd06301db0f6b9
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 18:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function nestedTarget(){
 
 function increaseRankBy(){
   let list = document.querySelectorAll('ul.ranked-list li')
+  console.log(list)
   for(var i = 0; i<list.length; i++){
     list[i].innerHTML = (i + 1).toString()
   }
