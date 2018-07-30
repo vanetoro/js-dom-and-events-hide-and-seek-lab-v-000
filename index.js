757adaf2265fa7b9ff818fb9dcdf73f0b9094b73
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 18:57:27 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(var i = 0; i<list.length; i++){
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n ).toString()
+  }
+
+}
+
+function deepestChild(){
+  return document.querySelector('div#grand-node div div div div')
+}
