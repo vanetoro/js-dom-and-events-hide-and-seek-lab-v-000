420beb25b234a08e68136c1c2a64a488b81b4eeb
Vanessa Toro <vanetoro84@gmail.com>
2018-07-30 18:47:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function increaseRankBy(n){
   let list = document.querySelectorAll('ul.ranked-list li')
   console.log(list)
   for(var i = 0; i<list.length; i++){
+    console.log(list[i])
     list[i].innerHTML = (i ).toString()
   }
 
